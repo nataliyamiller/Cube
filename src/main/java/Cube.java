8c856f9c507c94694cd45b9c72cd63afe96cafd7
Nataliya Miller & Stephanie Garcia <student@epicodus.com>
2015-08-19 10:01:30 -0700

    First test passes for Cube volume

---
+++ src/main/java/Cube.java
@@ -0,0 +1,13 @@
+public class Cube {
+  Rectangle mFace;
+
+  public Cube(Rectangle rectangle) {
+    mFace = rectangle;
+  }
+
+  public int volume() {
+    int length = mFace.getLength();
+    return length * length * length;
+  }
+
+}
